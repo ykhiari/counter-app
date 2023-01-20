cbab7eae17f78a57fccfb6eabfea6fc53aa399a0
ykhiari <yassine.khiari@outlook.com>
2023-01-20 22:26:00 +0100

    create counter using cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_counter_app/logic/cubit/counter_cubit.dart';
 
 void main() => runApp(const Counter());
 
@@ -7,38 +9,58 @@ class Counter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Counter App',
-      home: Scaffold(
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: ((context) => CounterCubit()))],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Counter App',
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FloatingActionButton(
-              onPressed: () {},
-              child: const Icon(Icons.add),
-            ),
+            const Text('Counter value:'),
+            Text(context.select(
+                (CounterCubit cubit) => cubit.state.counterValue.toString())),
             const SizedBox(
               height: 10,
             ),
-            FloatingActionButton(
-              onPressed: () {},
-              child: const Icon(Icons.remove),
-            ),
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Counter value:'),
-              Text('Counter'),
-              SizedBox(
-                height: 10,
-              ),
-            ],
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterCubit>().increment();
+            },
+            child: const Icon(Icons.add),
           ),
-        ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterCubit>().decrement();
+            },
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
