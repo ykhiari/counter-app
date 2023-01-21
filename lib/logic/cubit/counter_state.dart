9b84e74cc0c2acd5b89a433c89088e9c8faea1bb
ykhiari <yassine.khiari@outlook.com>
2023-01-21 16:39:04 +0100

    added local storage using hydrated bloc

--- lib/logic/cubit/counter_state.dart
+++ lib/logic/cubit/counter_state.dart
@@ -4,4 +4,23 @@ class CounterState {
   final int counterValue;
   final bool wasIncremented;
   CounterState({required this.counterValue, required this.wasIncremented});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'counterValue': counterValue,
+      'wasIncremented': wasIncremented,
+    };
+  }
+
+  factory CounterState.fromMap(Map<String, dynamic> map) {
+    return CounterState(
+      counterValue: map['counterValue']?.toInt() ?? 0,
+      wasIncremented: map['wasIncremented'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CounterState.fromJson(String source) =>
+      CounterState.fromMap(json.decode(source));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:simple_counter_app/data/themes/app_theme.dart';
 import 'package:simple_counter_app/logic/cubit/counter_cubit.dart';
 import 'package:simple_counter_app/view/pages/home_page.dart';
 
-void main() => runApp(const Counter());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
+  runApp(const Counter());
+}
 
 class Counter extends StatelessWidget {
   const Counter({super.key});
@@ -12,11 +19,14 @@ class Counter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [BlocProvider(create: ((context) => CounterCubit()))],
+      providers: [
+        BlocProvider(
+          create: ((context) => CounterCubit()),
+        ),
+      ],
       child: MaterialApp(
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.dark,
         debugShowCheckedModeBanner: false,
         title: 'Counter App',
         home: const HomePage(),
